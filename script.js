285555bde64f021b51b108c6d4ad936b0f6ff357
omar mohamed elsayed arafa abd alrazik amer <UG_31138937@commerce.tanta.edu.eg>
2024-12-08 19:30:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+  }
+  
+  function showAlert() {
+    alert('Thank you for your interest! Explore the sections below for more information.');
+  }
+  
